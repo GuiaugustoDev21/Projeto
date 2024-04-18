f1c9413cf172931143f0c3d53ab166a26f8664e3
GuiaugustoDev21 <guilhermeaugst18@gmail.com>
2024-04-18 20:42:58 -0300

    add Correcoes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function toggleMode() {
     const html = document.documentElement
 
-    if(html.classList.contains("light")) {
+    if (html.classList.contains("light")) {
         html.classList.remove("light");
     } else {
         html.classList.add("light")
     }
 
     const img = document.querySelector("#profile img");
-    
-    if (html.classList.contains("light")){
-        img.setAttribute("src", "./haha/img/avatar-light.png")
-    } else{
-        img.setAttribute("src", "./haha/img/avatar.png")
+
+    if (html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/img/avatar-light.png")
+    } else {
+        img.setAttribute("src", "./assets/img/avatar.png")
     }
 }
